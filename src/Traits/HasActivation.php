87f9daa0f5911a6477c5d5c8e064eed659109ac9
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-16 21:31:00 +0300

    Update

--- src/Traits/HasActivation.php
+++ src/Traits/HasActivation.php
@@ -2,21 +2,38 @@
 namespace Veneridze\LaravelUserActivation\Traits;
 use Activation;
 use Illuminate\Support\Carbon;
+use Veneridze\LaravelUserActivation\Events\Activated;
+use Veneridze\LaravelUserActivation\Events\Deactivated;
+use Veneridze\LaravelUserActivation\Events\NeedActivate;
 trait HasActivation {
     public function isActivated(): bool {
         return $this->activated_at != null;
     }
 
     public function deactivate(): void {
-        $this->activated_at = null;
-        $this->save();
+        if($this->isActivated()) {
+            $this->activated_at = null;
+            $this->save();
+            Deactivated::dispatch($this);
+        } else {
+            throw new \Exception("Учётная запись уже деактивирована");
+        }
     }
     public function activate(): void {
-        $this->activated_at = Carbon::now();
-        $this->save();
+        if(!$this->isActivated()) {
+            $this->activated_at = Carbon::now();
+            $this->save();
+            Activated::dispatch($this);
+        } else {
+            throw new \Exception("Учётная запись уже активирована");
+        }
     }
 
     public function sendActivation(): void {
-        
+        if(!$this->isActivated()) {
+            NeedActivate::dispatch($this);
+        } else {
+            throw new \Exception("Учётная запись уже активирована");
+        }
     }
 }
